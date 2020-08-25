6f2e7552f4d5ce3adb48f93d4d7e560ef2b7cf19
Saket Sharma <sharmasaket093@gmail.com>
2020-08-25 16:43:04 +0530

    Component Test

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -5,12 +5,12 @@ import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  constructor(private elem: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.renderer.addClass(this.el.nativeElement, 'highlight');
+    this.renderer.addClass(this.elem.nativeElement, 'highlight');
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.renderer.removeClass(this.el.nativeElement, 'highlight');
+    this.renderer.removeClass(this.elem.nativeElement, 'highlight');
   }
 }
